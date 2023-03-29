b6e0c95e3f5698c358151044d446c95e89b7e1ac
THANATACH <thanatuth.euro@gmail.com>
2023-03-29 20:40:13 +0700

    Change Saythanks logic to httpRequest

--- content_script.js
+++ content_script.js
@@ -5,43 +5,57 @@
  * @return {string|boolean} The value of the parameter, or false if it is not present in the URL.
  */
 function getUrlParameter(sParam) {
-    // Get the query string from the URL.
-    var queryString = window.location.search.substring(1);
-  
-    // Split the query string into an array of key-value pairs.
-    var queryParams = queryString.split('&');
-  
-    // Loop through the key-value pairs to find the requested parameter.
-    for (var i = 0; i < queryParams.length; i++) {
-      var keyValue = queryParams[i].split('=');
-      var paramName = keyValue[0];
-      var paramValue = keyValue[1] === undefined ? true : decodeURIComponent(keyValue[1]);
-  
-      if (paramName === sParam) {
-        // Return the value of the requested parameter.
-        return paramValue;
-      }
+  // Get the query string from the URL.
+  var queryString = window.location.search.substring(1);
+  // Split the query string into an array of key-value pairs.
+  var queryParams = queryString.split("&");
+
+  // Loop through the key-value pairs to find the requested parameter.
+  for (var i = 0; i < queryParams.length; i++) {
+    var keyValue = queryParams[i].split("=");
+    var paramName = keyValue[0];
+    var paramValue =
+      keyValue[1] === undefined ? true : decodeURIComponent(keyValue[1]);
+
+    if (paramName === sParam) {
+      // Return the value of the requested parameter.
+      return paramValue;
     }
-  
-    // The requested parameter was not found in the URL.
-    return false;
   }
 
-  chrome.storage.sync.get("options", function(items) {
-    // Check if there was no runtime error when retrieving the options from the storage
-    if (!chrome.runtime.error) {
-      // Check if the options were retrieved successfully
-      if(items.options){
-        // Log a message indicating that auto-thanks is activated on this page
-        console.log("Auto thanks on this page activated!");
-        // Call a function to send a 'say_thanks' request with the ID parameter from the URL
-        sndReq('action=say_thanks&id=' + getUrlParameter('id'), 'saythanks');
-        // Change the location of the page to 'javascript:void 0' to prevent the page from reloading
-        location.href = 'javascript:void 0';
-      }   
+  // The requested parameter was not found in the URL.
+  return false;
+}
+
+chrome.storage.sync.get("options", function (items) {
+  // Check if there was no runtime error when retrieving the options from the storage
+  if (!chrome.runtime.error) {
+    // Check if the options were retrieved successfully
+    if (items.options) {
+      // Log a message indicating that auto-thanks is activated on this page
+      console.log("Auto thanks on this page activated!");
+      // Call a function to send a 'say_thanks' request with the ID parameter from the URL
+      let idNumber = getUrlParameter('id');
+      requestSayThanks(idNumber)
+       
+      // Change the location of the page to 'javascript:void 0' to prevent the page from reloading
+      location.href = "javascript:void 0";
     }
-  });
-  
+  }
+});
 
- 
 
+function requestSayThanks(idNumber){
+const Http = new XMLHttpRequest();
+const url='/ajax.php? action=say_thanks&id=' + idNumber;
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+   if(Http.responseText.split("ขอขอบพระคุณที่ กดขอบคุณ").length > 1){
+
+    console.log('Auto thanks Comlete !');
+
+   }
+}
+}
